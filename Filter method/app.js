4836792f695677e14bfa4947a98fc40758b26fd7
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2022-12-24 12:18:49 +0500

    Map methods

---
+++ Filter method/app.js
@@ -0,0 +1,35 @@
+// const numbers = [4339,3920,1590,4320,1944,2393, 3420, 3042,2290, 4563, 4637,5000 ];
+
+// let output = []
+
+// numbers .forEach(item => {
+//     if(item % 39 == 0)
+//     output.push(item)
+// });
+// console.log(output);
+
+// // ----------------------------------------------------
+
+// let num = numbers.filter(item => {
+//     return (item % 2 == 0)
+   
+// });
+// console.log(num);
+
+// // ------------------------------------------------------
+
+// const workers = [
+//     {id: 53, name: 'Make Quin', age: 44, designation: 'tester', salary: 120000, tel: '+998993480829'},
+//     {id: 36, name: 'Tom Sawyer', age: 34, designation: 'science', salary: 2110000, tel: '+99899358789'},
+//     {id: 63, name: 'Cris Ron', age: 28, designation: 'tester', salary: 120000, tel: '+99899365843'},
+//     {id: 45, name: 'Messi Leo', age: 41, designation: 'developer', salary: 190000, tel: '+998993480829'},
+//     {id: 71, name: 'Golibboy', age: 39, designation: 'tester', salary: 120000, tel: '+998993897425'},
+//     {id: 55, name: 'Mike Tom', age: 45, designation: 'computer', salary: 1440000, tel: '+99899369547'},
+//     {id: 51, name: 'Melli Khan', age: 36, designation: 'tester', salary: 160000, tel: '+998993433254'}
+// ]
+
+
+// console.log(
+//     workers.filter(item => item.age < 29 && item.designation == 'tester')
+// );
+
